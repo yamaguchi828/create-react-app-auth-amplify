bbc4d74b87c1b4c7564143f9653ac94baf8e5b57
haniwa <narahaniwa828@gmail.com>
2022-09-19 12:15:12 +0900

    App クラスを変更

--- src/App.js
+++ src/App.js
@@ -13,17 +13,8 @@ class App extends Component {
         <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Sample App</h1>
+          <p>これは、サンプルで作成したアプリです。</p>
         </header>
       </div>
     );
